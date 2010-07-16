cae131b93a559dcd8a3980e6889309cff1869b49
giampaolo <giampaolo@a13ad8ac-ce12-0410-a602-c8c22c8f6e20>
2010-07-16 16:43:01 +0000

    git-svn-id: http://dev.inghist.nl/svn/BiografischPortaal/bioport_repository/trunk@17104 a13ad8ac-ce12-0410-a602-c8c22c8f6e20

--- bioport_repository/illustration.py
+++ bioport_repository/illustration.py
@@ -11,17 +11,13 @@ import simplejson
 
 from zLOG import LOG, INFO, ERROR, WARNING
 import PIL.Image
-from cStringIO import StringIO
 
 
 MEDIUM_THUMB_SIZE = (200, 200)
 SMALL_THUMB_SIZE = (100, 100)
 
-DEFAULT_WIDTH = 100
-DEFAULT_HEIGHT = 120
 
-def get_digest(astring):
-    return md5(astring).hexdigest()
+
 
 def logexception():
     msg = traceback.format_exc()
@@ -55,6 +51,7 @@ class Illustration:
         self._prefix = prefix
         self._link_url = link_url
         self._caption = caption
+        self._id = self._create_id()
 
 
     # --- public API used by the view
@@ -78,23 +75,32 @@ class Illustration:
         return self._url
 
     @property
-    def cached_url(self):  # XXX - l'url dell'immagine originale: da rinominare !!!
+    def image_url(self):  # XXX - l'url dell'immagine originale: da rinominare !!!
         """public url of the local copy of the image"""
-        return os.path.join(self._images_cache_url, self.create_id())
-        
+        return os.path.join(self._images_cache_url, self.id)
+       
     @property
     def image_medium_url(self):
+        """The url of the medium size thumbnail"""
         width, height = MEDIUM_THUMB_SIZE
-        return os.path.join(self._images_cache_url,  'thumbnails', '%ix%i_%s'
-                            % (width, height, self.create_id()))
+        return os.path.join(self._images_cache_url,  
+                            'thumbnails', 
+                            '%ix%i_%s' % (width, height, self.id))
 
     @property
     def image_small_url(self):
+        """The url of the small size thumbnail"""
         width, height = SMALL_THUMB_SIZE
-        return os.path.join(self._images_cache_url,  'thumbnails', '%ix%i_%s'
-                            % (width, height, self.create_id()))
+        return os.path.join(self._images_cache_url,  
+                            'thumbnails', 
+                            '%ix%i_%s' % (width, height, self.id))
+
 
     # --- public API used *internally* (not by the view) 
+    
+    @property
+    def id(self):
+        return self._id
 
     @property
     def images_directory(self):
@@ -107,32 +113,10 @@ class Illustration:
         return self._thumbnails_directory
         
     @property          
-    def cached_local(self):
+    def image_url(self):
         """path on the local file system to a copy of the image"""
-        return os.path.join(self._images_cache_local,  self.create_id())
+        return os.path.join(self.images_directory,  self.id)
     
-
-    def cached_thumbnail_local(self, width=DEFAULT_WIDTH, height=DEFAULT_HEIGHT):
-        return os.path.join(self._images_cache_local,  'thumbnails', '%ix%i_%s'
-                            % (width, height, self.create_id()))
-        
-    def thumbnail_url(self, width=DEFAULT_WIDTH, height=DEFAULT_HEIGHT):
-        """public url of the thumbnail of this image
-        If the thumbnail doesn't exist yet it wil be generated
-        """
-        if not self.has_thumbnail(width,height):
-            return ""
-            # XXX
-#            try:
-#	            self.create_thumbnail(width,height, refresh=True)
-#            except IOError: 
-                # probably because we cannot find the original file either
-                # we let this go, because we do not want the whole page to throw an error
-                # because of a broken link
-#                logexception()                
-        return "/".join((self._images_cache_url, 'thumbnails', '%ix%i_%s'
-                        % (width, height, self.create_id())))
-
     def download(self, overwrite=True):
         """Download the original image with original dimensions and saves it on
         disk.
@@ -140,12 +124,12 @@ class Illustration:
         Also, create two resized images which will then by used in the view,
         a medium and a smaller one, which will be saved im /thumbnails directory
         """
-        if not overwrite and os.path.exists(self.cached_local):
-            print 'XXX - image already exists at %s - no image downloaded' % self.cached_local
+        if not overwrite and os.path.exists(self.image_url):
+            print 'XXX - image already exists at %s - no image downloaded' % self.image_url
             return
 
         url = self.source_url
-        LOG('BioPort', INFO, 'downloading image from %s to %s' % (repr(url), repr(self.cached_local)))
+        LOG('BioPort', INFO, 'downloading image from %s to %s' % (repr(url), repr(self.image_url)))
 
         #  XXX - temporary
         """    
@@ -171,9 +155,9 @@ class Illustration:
                 return
         """
 
-        # write main image file on disk
+        # write main image file on disk 
         http = urllib2.urlopen(url)
-        with open(self.cached_local, 'w') as file:
+        with open(self.image_url, 'w') as file:
             file.write(http.read())
 
         # wirte two smaller thumbs on disk
@@ -189,38 +173,59 @@ class Illustration:
         """
         assert isinstance(width, int)
         assert isinstance(height, int)       
-        if not os.path.isfile(self.cached_local): 
+        if not os.path.isfile(self.image_url): 
               raise("the original image does not exists (it was supposed to be found here: %s)" 
-                     % self.cached_local)
+                     % self.image_url)
 
         # PIL stuff
         pilfilter = 0  # NEAREST
         if PIL.Image.VERSION >= "1.1.3":
             pilfilter = 1  # ANTIALIAS
-        image = PIL.Image.open(self.cached_local)
+        image = PIL.Image.open(self.image_url)
         image = image.convert('RGB')
         image.thumbnail((width, height), pilfilter)
 
         # write on disk
-        basename = os.path.basename(self.cached_local)
+        basename = os.path.basename(self.image_url)
         basename = "%sx%s_%s" % (width, height, basename)
         file = os.path.join(self.thumbnails_directory, basename)
         image.save(file, "JPEG", quality=88) 
-
-    def has_thumbnail(self, width=DEFAULT_WIDTH, height=DEFAULT_HEIGHT):
-        return os.path.isfile(self.cached_thumbnail_local(width, height))
-    
-    def create_id(self):
-        url = self._url
+   
+    def _create_id(self):
+        url = self.source_url
         filename = url.split('/')[-1]
         #create a unique hash on the basis of the url
         #(the hash code is created in this somewhat illogical way to maintian compatibility with earlier versions)
         url_to_hash = '/'.join(url.split('/')[:-1])
         if '?' in url:
             url_to_hash=url
+
+        def get_digest(astring):
+            return md5(astring).hexdigest()
+            
         digest = get_digest(url_to_hash)
         filename = filename.split('?')[0]
         filename = '%s_%s_%s' % (self._prefix, digest, filename)
         return filename
+
+
+
+    # --- deprecated attrs
+
+    @property
+    def cached_url(self):
+        raise ValueError("deprecated; use image_url instead")
+        
+    def create_id(self):
+        raise ValueError("deprecated; use _create_id instead")
+        
+    def has_thumbnail(self, *args, **kwargs):
+        raise ValueError("deprecated; not supposed to be u9sed")
+    
+    def thumbnail_url(self, *args, **kwargs):
+        raise ValueError("deprecated; not supposed to be used")
+
+    def cached_thumbnail_local(self, *args, **kwargs):
+        raise ValueError("deprecated; not supposed to be used")        
 
 
