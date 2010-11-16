8dc7846f208b388bbde14f062d21d4dfbde7dc74
jelle <jelle@a13ad8ac-ce12-0410-a602-c8c22c8f6e20>
2010-11-16 09:27:56 +0000

    fixed logging test
    git-svn-id: http://dev.inghist.nl/svn/BiografischPortaal/bioport_repository/trunk@19586 a13ad8ac-ce12-0410-a602-c8c22c8f6e20

--- bioport_repository/tests/test_logging.py
+++ bioport_repository/tests/test_logging.py
@@ -40,7 +40,7 @@ class LoggingTestCase(CommonTestCase):
         self.repo.add_source(source)
         self.assertEqual(self.last_log_message().table, 'source')
         self.repo.download_biographies(source)
-        self.assertEqual(self.last_log_message().table, 'biography')
+        self.assertEqual(self.last_log_message().table, 'source')
         #download biographies
         
         #change a biography
