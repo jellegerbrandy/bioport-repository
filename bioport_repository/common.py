994484f0065b62d0cc924fa004ca03f3b435faad
jelle <jelle@a13ad8ac-ce12-0410-a602-c8c22c8f6e20>
2010-12-07 14:38:38 +0000

    code cleanup
    git-svn-id: http://dev.inghist.nl/svn/BiografischPortaal/bioport_repository/trunk@19981 a13ad8ac-ce12-0410-a602-c8c22c8f6e20

--- bioport_repository/common.py
+++ bioport_repository/common.py
@@ -8,27 +8,32 @@ def to_date(s, round='down' ):
     round:
         one of ['down', 'up']: if 'down', take the lowest date, of 'up', take the max
         (i.e. to_date('2000', 'up') == datetime.datetime(2000, 1, 1))
-        """
+        
+    
+    """
     if not s:
         return s
     if len(s) == 4:
         format = '%Y'
         result = datetime.datetime.strptime(s, format)
         if round == 'up':
-	        result = result.replace(month=12, day=31)
+            result = result.replace(month=12, day=31)
     elif len(s) == 7:
         format = '%Y-%m'
         result = datetime.datetime.strptime(s, format)
         if round == 'up':
             if result.month == 12:
-		        result = result.replace(month=1, year=result.year + 1)
-		        result = result - datetime.timedelta(1)
+                result = result.replace(month=1, year=result.year + 1)
+                result = result - datetime.timedelta(1)
             else: 
-		        result = result.replace(month=result.month+1)
-		        result = result - datetime.timedelta(1)
+                result = result.replace(month=result.month+1)
+                result = result - datetime.timedelta(1)
     elif len(s) == 10:
         format = '%Y-%m-%d'
         result = datetime.datetime.strptime(s, format)
+    else:
+        raise ValueError('This data is not in ISO date format:%s' % s)
+        
     return result
 
 def format_date(d):
@@ -36,6 +41,6 @@ def format_date(d):
     
     (use this because strftime does not like dates before 1900)"""
     if d:
-	    return "%04d-%02d-%02d %02d:%02d" % (d.year,d.month,d.day,d.hour,d.minute)
+        return "%04d-%02d-%02d %02d:%02d" % (d.year,d.month,d.day,d.hour,d.minute)
     
     
