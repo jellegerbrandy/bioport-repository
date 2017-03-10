dfde2d06d199e00492d0149dd5989eccaccd8c51
Jelle Gerbrandy <github@gerbrandy.com>
2017-03-10 12:48:46 +0100

    fix typo

--- bioport_repository/datamanipulation/export_all_data.py
+++ bioport_repository/datamanipulation/export_all_data.py
@@ -9,12 +9,12 @@
 # it under the terms of the GNU General Public License as
 # published by the Free Software Foundation, either version 3 of the
 # License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public
 # License along with this program.  If not, see
 # <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -38,6 +38,7 @@ from bioport_repository.repository import Repository
 DIR_NAME = '/tmp/bioport_export/'
 TARFILE_NAME = '/tmp/bioport_export_%s.tar.gz' % unicode(datetime.date.today()).replace(' ', '_')
 DSN = 'mysql://root@localhost/bioport'
+
 
 def export_data(dsn):
     rr = repository = Repository(dsn=dsn)
@@ -69,4 +70,4 @@ def create_tar_file():
 if __name__ == "__main__":
     dsn = sys.argv[1]
     export_data(dsn)
-    create_tar_file()j
+    create_tar_file()
